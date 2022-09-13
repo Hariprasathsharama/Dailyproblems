fd2980f3e6675fd79370075a7447d6f5d689bc24
hariprasathsharama <hariprasathsharma956@gmail.com>
2022-09-13 17:54:57 +0530

    12/09/22

---
+++ Twosum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Twosum{
+            class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        Map <Integer,Integer>m=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            Integer no=(Integer)(target-nums[i]);
+            if(m.containsKey(no)){
+                   int retno[]={m.get(no),i};
+                    return retno;
+            }
+            m.put(nums[i],i);
+        }
+        return null;
+        
+    }
+} 
+}
+    
+
