ce11d20592dcd2dfe69727e7492e7d1045d19890
hariprasathsharama <hariprasathsharma956@gmail.com>
2022-09-20 17:13:16 +0530

    19/09/22

--- Minimax.java
+++ Minimax.java
@@ -1,4 +1,11 @@
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+// Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
+// Sum everything except 1, the sum is 2+3+4+5=14 .
+// Sum everything except 2, the sum is 1+3+4+5=13.
+// Sum everything except 3, the sum is 1+2+4+5=12.
+// Sum everything except 4 , the sum is 1+2+3+5=11.
+// Sum everything except 5 , the sum is 1+2+3+4=10.
 
 public class Minimax {
 
